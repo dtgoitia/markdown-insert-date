b614c6363235cb97c84956da98c51af9460bfc90
David Torralba Goitia <David-Torralba-Goitia@SoftwareImaging.com>
2019-09-02 13:31:32 +0100

    Initial commit

---
+++ spec/markdown-insert-date-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MarkdownInsertDateView from '../lib/markdown-insert-date-view';
+
+describe('MarkdownInsertDateView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
